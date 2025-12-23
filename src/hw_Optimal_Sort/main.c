3a2a3a82af71bb1621a3bb88ecbd0d6c4f6b0e7f
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-12-23 20:21:37 +0300

    Update main.c

--- src/hw_Optimal_Sort/main.c
+++ src/hw_Optimal_Sort/main.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
+#include "sort.h" 
 
-// Объявление функции
-extern int sort_array(int array[], int count);
-
-// Основной код - объявление массива на 100 чисел, создание счетчика, чтобы знать со сколькими числами надо будет работать, чтение чисел через for с scanf и вывод
-int main() {
+int main() 
+{
     int array[100];
     int count = 0;
-    
-    int i = 0;
-    while (i < 100 && scanf("%d", &array[i]) == 1) {
-        i++;
+
+    while (count < 100 && scanf("%d", &array[count]) == 1) {
         count++;
     }
-    
+
     int moved = sort_array(array, count);
-    
-    printf("Отсортированный массив:");
-    for (int i = 0; i < count; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-    return 0;
+
+    return moved;
 }
