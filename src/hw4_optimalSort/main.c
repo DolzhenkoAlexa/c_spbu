097ce336d4a60b872b38e3b83e0cd3ed82d1533c
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-12-24 12:16:43 +0300

    Update main.c

--- src/hw4_optimalSort/main.c
+++ src/hw4_optimalSort/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "sort.h" 
+#include "sort.h"
 
-int main() 
+int main()
 {
     int array[100];
     int count = 0;
@@ -12,5 +12,12 @@ int main()
 
     int moved = sortArray(array, count);
 
+    for (int i = 0; i < count; i++) {
+        // Чтоб удобнее глазками считать колво перестановок
+        printf("%d ", array[i]); 
+    }
+    printf("\n");
+
+    printf("%d ", moved);
     return moved;
 }
