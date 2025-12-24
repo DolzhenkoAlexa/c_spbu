7c96274197441f8de5141e6137aa9fce7f3833a0
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-12-24 12:14:57 +0300

    New logic for sort.c

--- src/hw4_optimalSort/sort.c
+++ src/hw4_optimalSort/sort.c
@@ -1,26 +1,36 @@
 #include "sort.h"
 
-int sortArray(int array[], int count) 
+int sortArray(int array[], int count)
 {
     int movedCount = 0;
-    
+
+    // Копия массива
+    int original[100];
+    for (int i = 0; i < count; i++) {
+        original[i] = array[i];
+    }
+
     for (int i = 0; i < count - 1; i++) {
-        int swapped = 0;  
-        
+        int swapped = 0;
         for (int j = 0; j < count - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-                movedCount++;
-                swapped = 1;  
+                swapped = 1;
             }
         }
-        
         if (!swapped) {
-            break;  
+            break;
         }
     }
-    
+
+    for (int i = 0; i < count; i++) {
+        if (array[i] != original[i]) {
+            movedCount++;
+        }
+    }
+
     return movedCount;
 }
+
