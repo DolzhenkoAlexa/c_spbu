bfc54aea420ab006d534b9b168c3e0a3c255480e
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2025-12-26 04:37:25 +0300

    Update sortList.c without main

--- src/hw6_sortList/sortList.c
+++ src/hw6_sortList/sortList.c
@@ -1,9 +1,6 @@
-//
-// Created by sasha on 19.11.2025.
-//
 #include <stdio.h>
-#include "sortList.h"
 #include <stdlib.h>
+#include "sortList.h"
 
 // Создание пустого списка
 struct List* createEmptyList()
@@ -18,227 +15,97 @@ struct List* createEmptyList()
     return list;
 }
 
-// Получение элемента по индексу
-int getElement(struct List* list, int i)
-{
-    // Проверка корректности индекса
-    if (i < 0 || i >= list->size) {
-        printf("Некорректный индекс: он не может быть < 0 или больше размера списка\n");
-        return -1;
-    }
-
-    struct Node* currNode = list->head;
-    while (i--) {
-        currNode = currNode->next;
-    }
-    return currNode->data;
-}
-
-// Реализация команды 0 - выйти:
-// Удаление всего списка для освобождения и выхода
+// Удаление всего списка
 void deleteList(struct List* list)
 {
-    struct Node* currNode = list->head;
-    // Удаление узлов
-    while (currNode) {
-        struct Node* tempNode = currNode;
-        currNode = currNode->next;
-        free(tempNode);
+    struct Node* curr = list->head;
+    while (curr) {
+        struct Node* tmp = curr;
+        curr = curr->next;
+        free(tmp);
     }
-    // Освобождение памяти
     free(list);
 }
 
-// Реализация команды 1 - добавить значение в сортированный список:
-// Поиск позиции для вставки элемента в список
-int findPosition(struct List* list, int element)
+// Вставка элемента
+void insertElement(struct List* list, int element)
 {
-    if (list->size == 0)
-        return 0;
-
-    struct Node* currNode = list->head;
-    int position = 0;
-
-    // Ищем первую позицию, где след элемент больше нового значения
-    // Так как список отсортирован, то прошлый будет или больше или равен вставляемому
-    while (currNode && currNode->data < element) {
-        currNode = currNode->next;
-        position++;
-    }
-    return position;
-}
-
-// Вставка элемента в список по заданному индексу
-void insertElement(struct List* list, int i, int element)
-{
-    // Проверка корректности индекса
-    if (i < 0 || i > list->size) {
-        printf("Некорректный индекс: он не может быть < 0 или больше размера списка\n");
-        return;
-    }
-
-    // Создание нового узла
     struct Node* newNode = malloc(sizeof(struct Node));
     if (!newNode) {
-        printf("Ошибка выделения памяти для саздания нового узла\n");
+        printf("Ошибка выделения памяти\n");
         return;
     }
     newNode->data = element;
-    // Вставка
-    if (i == 0) {
+    newNode->next = NULL;
+
+    if (!list->head || list->head->data >= element) {
         newNode->next = list->head;
         list->head = newNode;
-    } else {
-        // Поиск позиции для вставки
-        struct Node* currNode = list->head;
-        int pos = 1;
-        while (pos < i) {
-            currNode = currNode->next;
-            pos++;
-        }
-        newNode->next = currNode->next;
-        currNode->next = newNode;
+        list->size++;
+        return;
     }
+
+    // Поиск места
+    struct Node* curr = list->head;
+    while (curr->next && curr->next->data < element) {
+        curr = curr->next;
+    }
+
+    newNode->next = curr->next;
+    curr->next = newNode;
     list->size++;
 }
 
-// Реализация команды 2 – удалить значение из списка:
-// Проверка наличия значения в списке
-int checkElement(struct List* list, int element)
+// Удаление элемента
+int deleteElement(struct List* list, int element)
 {
-    struct Node* currNode = list->head;
-    int position = 0;
-
-    while (currNode) {
-        if (currNode->data == element) {
-            return position;
-        }
-        currNode = currNode->next;
-        position++;
+    if (!list->head) {
+        return 0;
     }
-    return -1;
-}
 
-// Удаление элемента по индексу
-void deleteElement(struct List* list, int i)
-{
-    // проверка корректности индекса
-    if (i < 0 || i >= list->size) {
-        printf("Некорректный индекс: он не может быть < 0 или больше размера списка\n");
-        return;
-    }
-    struct Node* nodeForDeletion;
-    // Удаление из начала списка
-    if (i == 0) {
-        nodeForDeletion = list->head;
+    if (list->head->data == element) {
+        struct Node* tmp = list->head;
         list->head = list->head->next;
-    } else {
-        // Поиск узла перед удаляемым
-        struct Node* currNode = list->head;
-        int pos = 1;
-        while (pos < i) {
-            currNode = currNode->next;
-            pos++;
-        }
-        nodeForDeletion = currNode->next;
-        currNode->next = nodeForDeletion->next;
+        free(tmp);
+        list->size--;
+        return 1;
     }
-    // Освобождение памяти
-    free(nodeForDeletion);
+
+    // Поиск элемента
+    struct Node* curr = list->head;
+    while (curr->next && curr->next->data != element) {
+        curr = curr->next;
+    }
+
+    if (!curr->next) {
+        return 0; // не найденн
+    }
+
+    struct Node* tmp = curr->next;
+    curr->next = tmp->next;
+    free(tmp);
     list->size--;
+    return 1;
 }
 
-// Реализация команды 3 – распечатать список:
-// Печать содержимого списка
+// Печать списка
 void printList(struct List* list)
 {
-    if (list->size == 0) {
-        printf("Список пустой\n");
+    if (!list->head) {
+        printf("Список пуст\n");
         return;
     }
 
-    struct Node* currNode = list->head;
-    printf("Содержание списка:\n");
+    struct Node* curr = list->head;
     printf("[");
-    while (currNode) {
-        printf("%d", currNode->data);
-        if (currNode->next) printf(", ");
-        currNode = currNode->next;
+    while (curr) {
+        printf("%d", curr->data);
+        if (curr->next) printf(", ");
+        curr = curr->next;
     }
     printf("]\n");
 }
 
-
-// Функция main для реализации выбора команд 0-3 пользователем
-int main()
-{
-    struct List* list = createEmptyList();
-    int command, inputElement;
-
-    while (1) {
-        printf("Команды:\n");
-        printf("0 - Выйти\n");
-        printf("1 - Добавить значение в сортированный список\n");
-        printf("2 - Удалить значение из списка\n");
-        printf("3 - Распечатать список\n");
-        printf("Выберите команду (0,1,2,3): ");
-
-
-        if (scanf("%d", &command) != 1) {
-            printf("Ошибка: вводите только цифры\n");
-            int ch;
-            while ((ch = getchar()) != '\n');
-
-            continue;
-        }
-
-        switch (command) {
-            case 0:
-                printf("Совершён выход\n");
-                deleteList(list);
-                return 0;
-
-            case 1:
-                printf("Введите значение для добавления: ");
-
-                if (scanf("%d", &inputElement) != 1) {
-                    printf("Ошибка: вводите только цифры\n");
-
-                    int ch;
-                    while ((ch = getchar()) != '\n');
-
-                    continue;
-                }
-
-                int position1 = findPosition(list, inputElement);
-                insertElement(list, position1, inputElement);
-                printf("Значение было добавлено на позицию %d\n", position1);
-                break;
-
-            case 2:
-                printf("Введите значение для удаления:");
-
-                if (scanf("%d", &inputElement) != 1) {
-                    printf("Ошибка: вводите только цифры\n");
-
-                    int ch;
-                    while ((ch = getchar()) != '\n');
-
-                    continue;
-                }
-
-                int position2 = checkElement(list, inputElement);
-                if (position2 != -1) {
-                    deleteElement(list, position2);
-                    printf("Значение было удалено с позиции %d\n", position2);
-                } else {
-                    printf("Значение не найдено\n");
-                }
-                break;
-
-            case 3:
-                printList(list);
-                break;
 
             default:
                 printf("Введена неподдерживаемая команда: можно только 0,1,2,3\n");
